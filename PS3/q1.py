181a82e33c08b5fa78c539aa51a7652597b3a067
suuuuu017 <suuuuu0177@outlook.com>
2022-10-31 14:40:06 -0400

    now acc is 98

--- PS3/q1.py
+++ PS3/q1.py
@@ -1,6 +1,7 @@
 from scipy import io
 import numpy as np
 from matplotlib import pyplot as plt
+from numpy import linalg
 import math
 
 def energy(label, beta, image, sig):
@@ -8,17 +9,17 @@ def energy(label, beta, image, sig):
     # secondPart = math.exp(-1 * np.dot(image, beta)) / (1 + math.exp(-1 * np.dot(image, beta)))
     # thirdPart = beta / sigma
 
-    deri = np.zeros((784, 1))
+    deri = np.zeros((785, 1))
 
-    for p in range(784):
+    for p in range(785):
         sum = 0
         for i in range(85):
-            sum = sum + ((label[i] - 1) + math.exp(-1 * np.dot(image[i], beta))
+            sum = sum + ((1 - label[i]) - math.exp(-1 * np.dot(image[i], beta))
                   / (1 + math.exp(-1 * np.dot(image[i], beta)))) * image[i][p]
-        sum = sum - beta[p] / sig
+        sum = sum + beta[p] / sig
         deri[p] = sum
 
-    deri = deri * -1
+    # deri = deri * -1
 
     return deri
 
@@ -28,6 +29,7 @@ if __name__ == '__main__':
     # the keys and values in data dict
     for key, _ in data.items():
         print(key)
+    input()
     image = data['trainX']
     label = data['trainY']
     print(image.shape)
@@ -38,23 +40,64 @@ if __name__ == '__main__':
 
     # filter out 0,1 data and label
     filterData = []
+    filterLabel = []
     for idx, x in enumerate(label):
         if x == 0 or x == 1:
             print(image[idx])
-            input()
-            filterData.append(image[idx])
+            # input()
+            newImg = np.append(image[idx], 1)
+            # print(newImg)
+            # input()
+            filterData.append(newImg)
+            filterLabel.append(x)
     filterData = np.array(filterData)
+    filterLabel = np.array(filterLabel)
     print("filtered data size is", filterData.shape)
 
     # gradient descent
-    beta = np.random.rand(784, 1)
+    beta = np.random.rand(785, 1)
     # print(beta)
 
     stepsize = 0.01
 
     # TODO: what should sigma be
-    sig = 0.001
+    sig = 1
 
-    for iter in range(500):
-        beta = beta + stepsize * energy(label, beta, filterData, sig)
-        print(beta)
+    for iter in range(50):
+        beta = beta - stepsize * energy(filterLabel, beta, filterData, sig)
+        print(np.linalg.norm(beta))
+
+    # predict
+    testImage = data['testX']
+    testLabel = data['testY']
+
+    # flatten the label
+    testLabel = np.squeeze(testLabel)
+    print("testLabel label is", np.squeeze(testLabel)[0])
+
+    # filter out 0,1 data and label
+    filterTestData = []
+    filterTestLabel = []
+    for idx, x in enumerate(testLabel):
+        if x == 0 or x == 1:
+            print(testImage[idx])
+            # input()
+            newImg = np.append(testImage[idx], 1)
+            # print(newImg)
+            # input()
+            filterTestData.append(newImg)
+            filterTestLabel.append(x)
+    filterTestData = np.array(filterTestData)
+    filterTestLabel = np.array(filterTestLabel)
+
+    acc = 0
+    for i in range(115):
+        l = 1 / (1 + math.exp(-1 * np.dot(filterTestData[i], beta)))
+        if l > 0.5:
+            l = 1
+        else:
+            l = 0
+        if l == filterTestLabel[i]:
+            acc = acc + 1
+
+    print("acc is ", acc / 116)
